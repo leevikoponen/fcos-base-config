06b18da422624040258286dfe791612c6f5fff53
Leevi Koponen <contact@leevikoponen.fi>
2024-01-03 09:32:15 +0200

    Make the conversion script work on systems with SELinux

--- util/run_butane_conversion.sh
+++ util/run_butane_conversion.sh
@@ -6,6 +6,7 @@ run_container() {
     podman run \
         --rm \
         --interactive \
+        --security-opt label=disable \
         --volume "$PWD:/pwd" \
         --workdir /pwd \
         "$@"
